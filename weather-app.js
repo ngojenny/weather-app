9e4ff2dec1151464458c6b567c0b146a0e3a9476
ngojenny <jennyngo.dev@gmail.com>
2016-05-24 12:27:58 -0400

    Initial Commit

---
+++ weather-app.js
@@ -0,0 +1,49 @@
+//c6e0884af486dcb5
+
+var weatherWidget = {
+};
+
+weatherWidget.apiURL ='http://api.wunderground.com/api/c6e0884af486dcb5/conditions/q/CA/San_Francisco.json';
+
+weatherWidget.init = function() {
+	//this code in here is used to initialize our application
+	weatherWidget.getData();
+};
+
+//When the page loads get some data
+//Make an AJAX call to the wundergrounds API
+weatherWidget.getData = function() {
+	$.ajax({
+		url: weatherWidget.apiURL,
+		method: 'GET',
+		dataType: 'json'
+	})
+	.then(function(weatherData){
+		// console.log(weatherData.current_observation);
+		var observation = weatherData.current_observation;
+		//pass the information along to the displayWeather so data represented by observation can be used down there V
+		weatherWidget.displayWeather(observation);
+	});
+};
+
+//when the data returns we want to display specific things (found on the html page) ('weather' is the data represented by observation above)
+weatherWidget.displayWeather = function(weather) {
+	console.log(weather);
+	var city = weather.display_location.city;
+	$('.city_name').text(city);
+	var temp_c = weather.temp_c;
+	console.log(temp_c);
+	$('.temp_c').text(temp_c);
+	var time = weather.local_time_rfc822;
+	console.log(time);
+	$('.date_time').text(time);
+	var condition = weather.weather;
+	$('.weather_string').text(condition);
+	var icon = weather.icon_url;
+	$('.weather_image').attr('src', icon);
+}
+
+
+$(document).ready(function(){
+  weatherWidget.init();
+});
